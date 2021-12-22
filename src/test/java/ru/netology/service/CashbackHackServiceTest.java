a5b364b3c08fcea8fc0321f176ba216a920159d3
Kate <pink-angel@mail.ru>
2021-12-22 16:13:15 +0300

    added junit4

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,34 +1,33 @@
 package ru.netology.service;
 
-import static org.testng.Assert.*;
-import org.testng.annotations.Test;
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
 
 public class CashbackHackServiceTest {
     CashbackHackService service = new CashbackHackService();
 
     @Test
     public void shouldCalculateCashbackHackServiceWithAmount800 (){
-        assertEquals(service.remain(800), 200);
+        assertEquals(200, service.remain(800));
     }
 
     @Test
     public void shouldCalculateCashbackHackServiceWithAmount1 (){
-        assertEquals(service.remain(1), 999);
+        assertEquals(999, service.remain(1));
     }
 
     @Test
     public void shouldCalculateCashbackHackServiceWithAmount1000() {
-            assertEquals(service.remain(1000), 0);
+            assertEquals(0, service.remain(1000));
     }
 
     @Test
     public void shouldCalculateCashbackHackServiceWithAmount1001() {
-        assertEquals(service.remain(1001), 999);
+        assertEquals(999, service.remain(1001));
     }
 
     @Test
     public void shouldCalculateCashbackHackServiceWithAmount1200() {
-        assertEquals(service.remain(1200), 800);
+        assertEquals(800, service.remain(1200));
     }
-
 }
